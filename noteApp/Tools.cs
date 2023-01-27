1a23bab590885dfed209dac0385f9c13444b6a11
adiiwill <gazdagadam02@gmail.com>
2023-01-27 19:58:55 +0100

    Added a WAIT_TIME constant to tools for a more comfortable adjustment

--- noteApp/Tools.cs
+++ noteApp/Tools.cs
@@ -8,6 +8,8 @@ namespace noteApp
 {
     internal class Tools
     {
+        private const int WAIT_TIME = 1400;
+
         public static void Wait(int ms)
         {
             Thread.Sleep(ms);
@@ -17,21 +19,21 @@ namespace noteApp
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"\n{message}");
             Console.ForegroundColor = ConsoleColor.Gray;
-            Wait(2000);
+            Wait(WAIT_TIME);
         }
         public static void Announcement(string message)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine($"\n{message}");
             Console.ForegroundColor = ConsoleColor.Gray;
-            Wait(2000);
+            Wait(WAIT_TIME);
         }
         public static void Success(string message)
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"\n{message}");
             Console.ForegroundColor = ConsoleColor.Gray;
-            Wait(2000);
+            Wait(WAIT_TIME);
         }
     }
 }
